a6a37b405d78c79ea86c8243715404854df95393
KrispehKreme <ericlstafford@gmail.com>
2026-05-16 12:03:45 -0400

    further than I meant to go before commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 from logger import log_state
+from asteroidfield import AsteroidField 
+from asteroid import Asteroid
 
 
 def main():
@@ -12,17 +14,27 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable, )
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
+        dt = clock.tick(60) / 1000
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        
 
 
 
